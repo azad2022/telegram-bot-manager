7ed60b1714bd390961c6fb0bfee25b4b56f52611
azad2022 <95263596+azad2022@users.noreply.github.com>
2025-10-07 20:36:32 +0330

    Update config.go

--- config.go
+++ config.go
@@ -5,13 +5,15 @@ type Config struct {
 	AdminID       int64
 	DatabaseURL   string
 	RedisURL      string
+	RedisPassword string
 }
 
 func LoadConfig() *Config {
 	return &Config{
 		TelegramToken: "8407008563:AAHBQpjUh60bHqpxOfAJqEfTmicNO6IfEl0",
 		AdminID:       269758292,
-		DatabaseURL:   "postgres://bot_user:bot_password_123@localhost:5433/telegram_bot",
+		DatabaseURL:   "postgres://bot_user:bot_password_123@localhost:5433/telegram_bot?sslmode=disable",
 		RedisURL:      "localhost:6380",
+		RedisPassword: "redis_password_123",
 	}
 }
