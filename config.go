04f1b7a258a720e45da24d008cd6df1bdce4c253
azad2022 <95263596+azad2022@users.noreply.github.com>
2025-10-07 19:11:00 +0330

    Create config.go

---
+++ config.go
@@ -0,0 +1,17 @@
+package main
+
+type Config struct {
+	TelegramToken string
+	AdminID       int64
+	DatabaseURL   string
+	RedisURL      string
+}
+
+func LoadConfig() *Config {
+	return &Config{
+		TelegramToken: "8407008563:AAHBQpjUh60bHqpxOfAJqEfTmicNO6IfEl0",
+		AdminID:       269758292,
+		DatabaseURL:   "postgres://bot_user:bot_password_123@localhost:5433/telegram_bot",
+		RedisURL:      "localhost:6380",
+	}
+}
