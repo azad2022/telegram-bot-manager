b891d5ee0220a5a49a2113a6f3be3b5dd3ad1f3e
azad2022 <95263596+azad2022@users.noreply.github.com>
2025-10-09 09:05:48 +0330

    Update config.go

--- config.go
+++ config.go
@@ -1,19 +1,19 @@
 package main
 
+import (
+	"os"
+)
+
 type Config struct {
-	TelegramToken string
-	AdminID       int64
-	DatabaseURL   string
-	RedisURL      string
-	RedisPassword string
+	BotToken    string
+	DatabaseURL string
 }
 
-func LoadConfig() *Config {
-	return &Config{
-		TelegramToken: "8407008563:AAHBQpjUh60bHqpxOfAJqEfTmicNO6IfEl0",
-		AdminID:       269758292,
-		DatabaseURL:   "postgres://bot_user:bot_password_123@localhost:5433/telegram_bot?sslmode=disable",
-		RedisURL:      "localhost:6380",
-		RedisPassword: "redis_password_123",
+func LoadConfig() Config {
+	return Config{
+		BotToken: os.Getenv("BOT_TOKEN"),
+
+		// ✅ کانکشن درست به دیتابیس اصلی با پورت استاندارد
+		DatabaseURL: "postgres://bot_user:bot_password_123@localhost:5432/telegram_bot_manager?sslmode=disable",
 	}
 }
